523cbef9a911679502a57b7c7409ad6bbfa9ea7f
Jiale-Lyu <113040940+Jiale-Lyu@users.noreply.github.com>
2023-05-31 13:30:32 -0400

    Add files via upload

---
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/neu/springcoredemo/common/Coach.java
@@ -0,0 +1,6 @@
+package com.neu.springcoredemo.common;
+
+public interface Coach {
+
+    String getDailyWorkout();
+}
---
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/neu/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,33 @@
+package com.neu.springcoredemo.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+
+//Component annotation marks the class as a Spring Bean, makes it available for dependency injection
+public class CricketCoach implements Coach{
+
+
+    public CricketCoach(){
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void doMyCleanupStuff(){
+        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 mins!;-)";
+    }
+}
